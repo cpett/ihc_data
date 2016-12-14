61f0d57e94f3deba1c04b4978ce3411be0d41c1a
Cody Pettit <cpett26@gmail.com>
2016-12-14 16:42:39 +0000

    Allow negative values in response url

--- ihc/urls.py
+++ ihc/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     url(r'^dashboard/', views.dashboard, name='dashboard'),
     url(r'^vizes/(?P<viz>[\w.@+-]+)', views.vizes, name='vizes'),
     url(r'^project_management/', views.project_management, name='project_management'),
-    url(r'^response/(?P<val>\d+\.\d{2})', views.response, name='response'),
+    url(r'^response/(?P<val>-?\d+\.\d{2})', views.response, name='response'),
 ]
